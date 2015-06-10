f43d59d23a9a4a38d1ddafb5ab7a77f56898072f
GilDev <gildev@gmail.com>
2015-06-10 22:58:35 +0200

    Added German localisation Error when "zorgen" = "zörgen", no idea why

--- src/localization.c
+++ src/localization.c
@@ -3,28 +3,30 @@
 
 void initLocales(void)
 {
-	char *locale = setlocale(LC_ALL, "");
-	ResHandle localeFile;
+	char *locale = setlocale(LC_ALL, "de_DE");
 
+	ResHandle localeFile;
 	if (strcmp("fr_FR", locale) == 0) {
 		localeFile = resource_get_handle(RESOURCE_ID_FRENCH_LOCALE);
+	} else if (strcmp("de_DE", locale) == 0) {
+		localeFile = resource_get_handle(RESOURCE_ID_GERMAN_LOCALE);
 	} else {
 		localeFile = resource_get_handle(RESOURCE_ID_ENGLISH_LOCALE);
 	}
 
-	int fileSize = resource_size(localeFile);
+	size_t fileSize = resource_size(localeFile);
 
 	uint8_t *strings = malloc(fileSize);
 	resource_load(localeFile, strings, fileSize);
 
-	for (int i = 0, strCurrentId = 0, strStartPosition = 0; i < fileSize; i++) {
+	for (uint16_t i = 0, strCurrentId = 0, strStartPosition = 0; i < fileSize; i++) {
 		switch (strings[i]) {
 			case '|':
 				strings[i] = '\n';
 				break;
 			case '\n':
 				locale = malloc(i - strStartPosition);
-				strncpy(locale, (char *) &strings[strStartPosition], i - strStartPosition);
+				memcpy(locale, &strings[strStartPosition], i - strStartPosition);
 				locale[i - strStartPosition] = '\0';
 				str[strCurrentId++] = locale;
 				strStartPosition = i + 1;
